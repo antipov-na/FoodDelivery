99bf6658daa3535d12cb22ce3eb4c925dad5cd6e
Nikita <suntipov@gmail.com>
2021-12-31 21:06:29 +0300

    removed interface for ShopItem

--- Entities/FoodItem.cs
+++ Entities/FoodItem.cs
@@ -2,7 +2,7 @@
 
 namespace Domain.Entities
 {
-    public class FoodItem : IShopItem
+    public class FoodItem
     {
         public int Id { get; set; }
         public string Name { get; set; }
