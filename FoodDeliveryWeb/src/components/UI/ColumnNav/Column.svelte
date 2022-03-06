d640f598e4675d61eb242e01b08498c5ff85d955
Nikita <suntipov@gmail.com>
2022-03-06 20:39:41 +0300

    added ColumnNav component

---
+++ FoodDeliveryWeb/src/components/UI/ColumnNav/Column.svelte
@@ -0,0 +1,23 @@
+<script>
+</script>
+
+<div class="column">
+    <slot name="header" />
+    <ul class="links">
+        <slot />
+    </ul>
+</div>
+
+<style>
+    .column {
+        min-width: var(--column-min-width, 0);
+        max-width: var(--column-max-width, unset);
+    }
+
+    .links {
+        margin: 0;
+        display: flex;
+        flex-direction: column;
+        gap: var(--links-gap, 5px);
+    }
+</style>
---
+++ FoodDeliveryWeb/src/components/UI/ColumnNav/ColumnNav.svelte
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<div class="columnNav">
+    <slot />
+</div>
+
+<style>
+    .columnNav {
+        display: flex;
+        background-color: var(--background-color, unset);
+        padding: var(--padding, 0);
+        border-radius: var(--border-radius, 0);
+        gap: var(--column-gap, 0);
+    }
+</style>
